0311301d1ff7e63096dc8e82a0f87b5ae1cdd98b
cjcjon <cjcjon30@gmail.com>
2023-01-28 15:27:24 +0900

    feat: 카프카 동기 컨슘

--- src/main/kotlin/chapter3/SimpleConsumer.kt
+++ src/main/kotlin/chapter3/SimpleConsumer.kt
@@ -1,7 +1,10 @@
 package chapter3.chapter3
 
 import chapter3.logger
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
+import org.apache.kafka.common.TopicPartition
 import util.KafkaProperties
 import java.time.Duration
 
@@ -15,6 +18,40 @@ class SimpleConsumer {
     while (true) {
       val records = consumer.poll(Duration.ofSeconds(1))
       records.forEach { logger().info("{}", it) }
+    }
+  }
+
+  fun consumeSync(): Unit {
+    val configs = KafkaProperties.stringConsumer(groupId = GROUP_ID)
+    configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+
+    val consumer = KafkaConsumer<String, String>(configs)
+    consumer.subscribe(listOf(TOPIC_NAME))
+
+    while (true) {
+      val records = consumer.poll(Duration.ofSeconds(1))
+      records.forEach { logger().info("{}", it) }
+      consumer.commitSync()
+    }
+  }
+
+  fun consumeSyncOneByOne(): Unit {
+    val configs = KafkaProperties.stringConsumer(groupId = GROUP_ID)
+    configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+
+    val consumer = KafkaConsumer<String, String>(configs)
+    consumer.subscribe(listOf(TOPIC_NAME))
+
+    while (true) {
+      val records = consumer.poll(Duration.ofSeconds(1))
+      records.forEach {
+        logger().info("{}", it)
+        consumer.commitSync(
+          mapOf(
+            TopicPartition(it.topic(), it.partition()) to OffsetAndMetadata(it.offset() + 1)
+          )
+        )
+      }
     }
   }
 
